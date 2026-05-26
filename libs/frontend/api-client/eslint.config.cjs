603bf68712b7e2b4b442640301e328f7888fbb74
NMI <66474195+nmime@users.noreply.github.com>
2026-05-26 10:00:18 +0500

    chore: remove relocated api-client eslint config

--- libs/frontend/api-client/eslint.config.cjs
+++
@@ -1,8 +0,0 @@
-const base = require("../../../eslint.config.js");
-
-module.exports = [
-  {
-    ignores: ["lib/src/generated/**", "libs/frontend/api-client/lib/src/generated/**"],
-  },
-  ...base,
-];

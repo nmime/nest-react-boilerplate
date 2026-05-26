00b895dd0750264ab7b500d556ff3dfd27a4d437
NMI <66474195+nmime@users.noreply.github.com>
2026-05-26 13:07:15 +0500

    chore: remove stale oauth library

--- libs/feature/auth/oauth/lib/src/lib/auth-oauth.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { AuthOAuthService } from "./auth-oauth.service";
-
-@Module({
-  providers: [AuthOAuthService],
-  exports: [AuthOAuthService],
-})
-export class AuthOAuthModule {}

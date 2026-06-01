da2756580394e37183c55472be2a04506bfccad1
nmime <66474195+nmime@users.noreply.github.com>
2026-06-01 09:30:20 +0500

    Expose auth token cleanup service from auth postgres package

--- libs/postgres/main/auth/lib/src/index.ts
+++ libs/postgres/main/auth/lib/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./lib/auth-postgres.module";
+export * from "./lib/auth-token-cleanup.service";
 export * from "./lib/entity";
 export * from "./lib/repository";
 export * from "./lib/migrations";
